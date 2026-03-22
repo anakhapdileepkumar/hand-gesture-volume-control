079079469a44e87f2eee4cce7a2efd5c62fbc9bd
anakhapdileepkumar <anakhapdileep20@gmail.com>
2026-03-22 13:01:50 +0530

    Hand detection using MediaPipe

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import cv2
+import mediapipe as mp
+
+# Initialize MediaPipe
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+
+mp_draw = mp.solutions.drawing_utils
 
 # Start webcam
 cap = cv2.VideoCapture(0)
@@ -7,15 +14,24 @@ while True:
     success, img = cap.read()
 
     if not success:
-        print("Camera not working")
         break
 
-    cv2.imshow("Webcam Feed", img)
+    # Convert BGR → RGB
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # Press 'q' to exit
+    # Process image
+    results = hands.process(imgRGB)
+
+    # Check if hand is detected
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            # Draw landmarks
+            mp_draw.draw_landmarks(img, handLms, mp_hands.HAND_CONNECTIONS)
+
+    cv2.imshow("Hand Detection", img)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Cleanup
 cap.release()
 cv2.destroyAllWindows()
